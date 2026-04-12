b814201856380d386bdd4a98f4ecb3ef602d3738
DreamerOnMaybe <omorfi1337@yandex.ru>
2026-04-12 21:44:03 +0300

    added:changing the avatar from the offered options

--- my-app/src/pages/Profile.jsx
+++ my-app/src/pages/Profile.jsx
@@ -1,109 +1,186 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { db } from '../firebase.js'
-import { doc, getDoc, setDoc } from 'firebase/firestore'
+import { db } from "../firebase.js";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase"; // Импорт конфига Firebase
 import { signOut } from "firebase/auth"; // Импорт функции выхода
-import "./Profile.css"
-import Header from "../components/Header.jsx"
-import Navbar from "../components/Navbar.jsx"
+import "./Profile.css";
+import Header from "../components/Header.jsx";
+import Navbar from "../components/Navbar.jsx";
 
-import defaultAvatar from "../assets/account.png"
+import AvatarModal from "../components/AvatarModal.jsx";
+
+import defaultAvatar from "../assets/account.png";
 
 function Profile() {
+  const [avatar, setAvatar] = useState(null)
+  const [isAvatarModalOpen, setIsAvatarModalOpen] = useState(false);
+
+  const handleSelectAvatar = async (avatarSrc) => {
+    setAvatar(avatarSrc);
+    await setDoc(
+      doc(db, "users", userId),
+      { avatar: avatarSrc },
+      { merge: true },
+    );
+  };
+
+  const getWeekDays = () => {
+    const today = new Date();
+    const days = [];
+
+    //Находим понедельник текущей недели в monday в dayOfWeek мы получаем сегодняшний день
+    const monday = new Date(today);
+    const dayOfWeek = today.getDay();
+    const diff = dayOfWeek === 0 ? -6 : 1 - dayOfWeek; // today.getDay() возвращает число от 0 до 6, где 0 это воскресенье, 1 понедельник, 2 вторник и так далее. Например сегодня среда — getDay() вернёт 3. Чтобы найти понедельник нужно отнять 2 дня назад. Формула 1 - dayOfWeek как раз это и считает: 1 - 3 = -2.
+    monday.setDate(today.getDate() + diff); // Если воскресенье — getDay() вернёт 0, и формула даст 1 - 0 = 1 что неправильно, поэтому для воскресенья особый случай -6.
+    // monday.setDate(today.getDate() + diff) — просто берём сегодняшнее число и прибавляем diff. Если сегодня 19 и diff = -2, получаем 17 — это и есть понедельник.
+
+    //Генерируем 7 дней начиная с понедельника
+    for (let i = 0; i < 7; i++) {
+      const day = new Date(monday);
+      day.setDate(monday.getDate() + i);
+      days.push(day);
+    }
+    // i начинается с 0 и увеличивается до 6 — семь раз. Каждый раз создаём новый день от понедельника + i дней, и пушим в массив. Получаем 7 дней от понедельника до воскресенья.
+    return days;
+  };
+
+  const formatDay = (date) => {
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear();
+    return `${day}.${month}.${year}`;
+  };
+
+  const isDayCompleted = (date) => {
+    const dateKey = formatDay(date);
+    return habits.some((habit) => habit.log?.[dateKey]?.completed); // проверяем, есть ли в логах привычки, которые были выполнены в этот день
+  };
+
+  const [habits, setHabits] = useState([]);
+
   const [stats, setStats] = useState({
     habitsCompleted: 0,
     tasksCompleted: 0,
     streak: 0,
-  })
+  });
 
-  const [isEditing, setIsEditing] = useState(false) // состояние редактирования текста профиля
-  const [editData, setEditData] = useState({ name: '', bio: '' }) // состояние для изменений
+  const [isEditing, setIsEditing] = useState(false); // состояние редактирования текста профиля
+  const [editData, setEditData] = useState({ name: "", bio: "" }); // состояние для изменений
 
-  const [userId, setUserId] = useState(null) // состояние для id текущего пользователя
-  const [userData, setUserData] = useState({ name: '', bio: '' }) // состояние для данных профиля - имя и био
+  const [userId, setUserId] = useState(null); // состояние для id текущего пользователя
+  const [userData, setUserData] = useState({ name: "", bio: "" }); // состояние для данных профиля - имя и био
 
-  const handleSave = async () => { // функция сохранения
-    await setDoc(doc(db, 'users', userId), {
-      name: editData.name,
-      bio: editData.bio
-    }, { merge: true })
-    setUserData(editData)
-    setIsEditing(false)
-  }
+  const handleSave = async () => {
+    // функция сохранения
+    await setDoc(
+      doc(db, "users", userId),
+      {
+        name: editData.name,
+        bio: editData.bio,
+      },
+      { merge: true },
+    );
+    setUserData(editData);
+    setIsEditing(false);
+  };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => { // слушаем firebase auth - когда пользователь загрузился, сохраняем его uid- уникальный идентификатор пользователя
-      if (currentUser) setUserId(currentUser.uid)
-    })
-    return () => unsubscribe() // отписываемся когда компонент удаляется
-  }, [])
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      // слушаем firebase auth - когда пользователь загрузился, сохраняем его uid- уникальный идентификатор пользователя
+      if (currentUser) setUserId(currentUser.uid);
+    });
+    return () => unsubscribe(); // отписываемся когда компонент удаляется
+  }, []);
 
-  useEffect(() => { // загружаем данные профиля из Firestore когда узнали userId, зависимость userId запускается только когда он изменился
-    if (!userId) return // ждем пока не узнаем пользователя
+  useEffect(() => {
+    // загружаем данные профиля из Firestore когда узнали userId, зависимость userId запускается только когда он изменился
+    if (!userId) return; // ждем пока не узнаем пользователя
     const loadProfile = async () => {
-      const docSnap = await getDoc(doc(db, 'users', userId))
+      const docSnap = await getDoc(doc(db, "users", userId));
       if (docSnap.exists()) {
-        const data = docSnap.data()
+        const data = docSnap.data();
+        setAvatar(data.avatar || null)
         setUserData({
-          name: data.name || '', // если нет имени - пустая строка
-          bio: data.bio || ''
-        })
-        const habits = data.habits || [] // берем привычки, если нет - пустой массив
-        const tasks = data.tasks || {} // берем задачи, если нет - пустой объект
+          name: data.name || "", // если нет имени - пустая строка
+          bio: data.bio || "",
+        });
+        const habits = data.habits || []; // берем привычки, если нет - пустой массив
+        const tasks = data.tasks || {}; // берем задачи, если нет - пустой объект
 
-        const habitsCompleted = habits.filter(h => h.minutes > 0).length // фильтруем привычки, если минут больше 0 - значит пользователь уделил им время
+        const habitsCompleted = habits.filter((h) => h.minutes > 0).length; // фильтруем привычки, если минут больше 0 - значит пользователь уделил им время
 
         const tasksCompleted = Object.values(tasks)
           .flat()
-          .filter(t => t.completed).length
+          .filter((t) => t.completed).length;
 
-        setStats({ habitsCompleted, tasksCompleted, streak: data.streak || 0})
+        setStats({ habitsCompleted, tasksCompleted, streak: data.streak || 0 });
+        setHabits(data.habits || []);
       }
-    }
-    loadProfile()
-  }, [userId])
+    };
+    loadProfile();
+  }, [userId]);
 
   const handleSignOut = async () => {
     await signOut(auth);
   };
 
-
   return (
     <div className="app-container">
       <Header
-        title={auth.currentUser?.displayName || 'Профиль'}
+        title={auth.currentUser?.displayName || "Профиль"}
         onRightClick={handleSignOut}
       />
 
       <div className="profile-card">
-        <div className="profile-avatar">
-          <img src={defaultAvatar} alt="" />
+        <div
+          className="profile-avatar"
+          onClick={() => setIsAvatarModalOpen(true)}
+        >
+          <img src={avatar || defaultAvatar} alt="" />
+          <div className="avatar-edit">📷</div>
         </div>
         <div className="profile-info">
           {isEditing ? (
             <>
               <input
                 value={editData.name}
-                onChange={e => setEditData({...editData, name: e.target.value})}
+                onChange={(e) =>
+                  setEditData({ ...editData, name: e.target.value })
+                }
                 placeholder="Ваше имя"
               />
-              <input  
+              <input
                 value={editData.bio}
-                onChange={e => setEditData({...editData, bio: e.target.value})}
+                onChange={(e) =>
+                  setEditData({ ...editData, bio: e.target.value })
+                }
                 placeholder="О себе"
               />
               <div className="set-editing-btns">
-                <button className="close" onClick={() => setIsEditing(false)}>Отмена</button>
+                <button className="close" onClick={() => setIsEditing(false)}>
+                  Отмена
+                </button>
                 <button onClick={handleSave}>Сохранить</button>
               </div>
             </>
-          ): (
+          ) : (
             <>
-              <h2 className="profile-name">{userData.name || 'Без имени'}</h2>
-              <p className="profile-desc">{userData.bio || 'Добавьте описание'}</p>
-              <button className="editing-btn" onClick={() => { setEditData(userData); setIsEditing(true) }}>✏️</button>
+              <h2 className="profile-name">{userData.name || "Без имени"}</h2>
+              <p className="profile-desc">
+                {userData.bio || "Добавьте описание"}
+              </p>
+              <button
+                className="editing-btn"
+                onClick={() => {
+                  setEditData(userData);
+                  setIsEditing(true);
+                }}
+              >
+                ✏️
+              </button>
             </>
           )}
         </div>
@@ -127,7 +204,30 @@ function Profile() {
         </div>
       </div>
 
-      <Navbar activeTab='profile' isChatCenter={true} />
+      <div className="week-habits">
+        <h3>Привычки на неделе</h3>
+        <div className="week-circles">
+          {getWeekDays().map((day, index) => (
+            <div key={index} className="week-day">
+              <div
+                className={`habit-circle ${isDayCompleted(day) ? "completed" : ""}`}
+              >
+                {isDayCompleted(day) && <span>✓</span>}
+              </div>
+              <span>{["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"][index]}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {isAvatarModalOpen && (
+        <AvatarModal
+          onSelect={handleSelectAvatar}
+          onClose={() => setIsAvatarModalOpen(false)}
+        />
+      )}
+
+      <Navbar activeTab="profile" isChatCenter={true} />
     </div>
   );
 }
